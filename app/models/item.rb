3eb4d2556c61b20af1c501de444c09b863723efe
瀬戸ちゃこ <setochako@setochakonoMacBook-Air.local>
2025-07-24 01:00:22 +0900

    カテゴリ追加と画像または名前必須設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,23 @@
 class Item < ApplicationRecord
-    has_one_attached :image 
-    CATEGORIES = ['トップス', 'パンツ', 'スカート', 'ワンピース', 'コート', '小物', 'アクセサリー', 'バッグ']
-    COLORS = ['白', '黒', 'グレー', '赤', '青', '緑', '黄', 'ピンク', 'ベージュ', 'ブラウン']
+has_one_attached :image 
+CATEGORIES = ['トップス', 'パンツ', 'スカート', 'ワンピース', 'コート', '小物', 'アクセサリー', 'バッグ', '靴']
+COLORS = ['白', '黒', 'グレー', '赤', '青', '緑', '黄', 'ピンク', 'ベージュ', 'ブラウン']
   
-    validates :name, presence: true
-    # 他のバリデーションもここに
+validate :name_or_image_present
+
+private
+
+def name_or_image_present
+  if name.blank? && !image.attached?
+    errors.add(:base, "名前または画像のどちらかは必須です")
   end
+end
+    # 他のバリデーションもここに
+    private
+
+  def name_or_image_present
+    if name.blank? && !image.attached?
+      errors.add(:base, "名前または画像のいずれかは必須です")
+    end
+  end
+end
