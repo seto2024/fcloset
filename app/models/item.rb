1dbc9f511b45d2206aaad58fa09a85fe8f2728d2
瀬戸ちゃこ <setochako@setochakonoMacBook-Air.local>
2025-07-24 00:32:33 +0900

    カラーとカテゴリは登録プルダウンに

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
     has_one_attached :image 
+    CATEGORIES = ['トップス', 'パンツ', 'スカート', 'ワンピース', 'コート', '小物', 'アクセサリー', 'バッグ']
+    COLORS = ['白', '黒', 'グレー', '赤', '青', '緑', '黄', 'ピンク', 'ベージュ', 'ブラウン']
   
     validates :name, presence: true
     # 他のバリデーションもここに
