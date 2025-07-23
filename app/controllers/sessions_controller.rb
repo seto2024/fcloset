799bf8ad2911a94cf8eb00911aa3dff2cd89d49c
瀬戸ちゃこ <setochako@setochakonoMacBook-Air.local>
2025-07-24 01:38:06 +0900

    ログアウト修正も兼ねて

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_action :require_login, only: [:new, :create]
+  skip_before_action :require_login, only: [:new, :create, :destroy]
 
     def new
     end
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
   
     def destroy
       logout
-      redirect_to root_path, notice: 'ログアウトしました'
+      redirect_to login_path, notice: 'ログアウトしました'
     end
   end
