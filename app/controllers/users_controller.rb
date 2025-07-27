1d98af3aa5a958fea56e840a6a7d8729299aeaa0
瀬戸ちゃこ <setochako@setochakonoMacBook-Air.local>
2025-07-27 17:05:18 +0900

    ユーザーコントローラー編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: [:new, :create]
+  before_action :authenticate_user!, except: [:new, :create]
 
   def new
     @user = User.new
