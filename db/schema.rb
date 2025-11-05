f9ccfa4a97f2e4364dbdb038a862d92cb2f1ee02
瀬戸ちゃこ <setochako@setochakonoMacBook-Air.local>
2025-11-06 07:21:56 +0900

    お気に入り機能エラー対応

--- db/schema.rb
+++ db/schema.rb
@@ -96,10 +96,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_05_104108) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "reset_password_token"
+    t.boolean "first_login", default: true
     t.string "encrypted_password", default: "", null: false
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
-    t.boolean "first_login", default: true
     t.bigint "theme_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
